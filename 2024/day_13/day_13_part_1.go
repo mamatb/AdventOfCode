b6c252830f3db109312994b5d861d356e224cca9
mamatb <amatbaeza@gmail.com>
2024-12-17 20:58:41 +0100

    advent of code 2024, day 13 part 1

---
+++ 2024/day_13/day_13_part_1.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func isInt(num float64) bool {
+	return float64(int(num)) == num
+}
+
+func solveEquations(equationA []float64, equationB []float64) (float64, float64) {
+	x := (equationA[1]*equationB[2] - equationB[1]*equationA[2]) /
+		(equationA[1]*equationB[0] - equationB[1]*equationA[0])
+	y := (equationA[2] - equationA[0]*x) / equationA[1]
+	return x, y
+}
+
+func main() {
+	var inputScanner *bufio.Scanner
+	if input, err := os.Open("day_13.txt"); err == nil {
+		defer input.Close()
+		inputScanner = bufio.NewScanner(input)
+	} else {
+		panic(err)
+	}
+	opRegexp, err := regexp.Compile(`[^:]+: X[\+=]([0-9]+), Y[\+=]([0-9]+)`)
+	if err != nil {
+		panic(err)
+	}
+	tokens := 0.0
+	for inputScanner.Scan() {
+		equationA, equationB := []float64{}, []float64{}
+		for i := 0; i < 3; i++ {
+			operators := opRegexp.FindStringSubmatch(inputScanner.Text())[1:]
+			if op, err := strconv.ParseFloat(operators[0], 64); err == nil {
+				equationA = append(equationA, op)
+			} else {
+				panic(err)
+			}
+			if op, err := strconv.ParseFloat(operators[1], 64); err == nil {
+				equationB = append(equationB, op)
+			} else {
+				panic(err)
+			}
+			inputScanner.Scan()
+		}
+		pushesA, pushesB := solveEquations(equationA, equationB)
+		if isInt(pushesA) && isInt(pushesB) {
+			tokens += pushesA*3 + pushesB
+		}
+	}
+	fmt.Println(int(tokens))
+}
