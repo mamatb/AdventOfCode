70c3f41c31e0ca5c84b813a1d57604b15106de4d
mamatb <amatbaeza@proton.me>
2025-12-23 10:07:27 +0100

    advent of code 2025, day 4 part 2

--- 2025/day_4/day_4_part_2.go
+++ 2025/day_4/day_4_part_2.go
@@ -28,26 +28,23 @@ func outOfBounds(pos position, rows int, cols int) bool {
 	return pos.row < 0 || pos.row >= rows || pos.col < 0 || pos.col >= cols
 }
 
-func countAccessible(grid [][]bool) int {
+func countAccessible(grid [][]bool, rolls map[position]bool) int {
 	accessible := 0
-	for rowIdx, row := range grid {
-		for colIdx, roll := range row {
-			if !roll {
-				continue
+	for roll := range rolls {
+		adjacent := 0
+		for _, pos := range getAdjacent(position{roll.row, roll.col}) {
+			if !outOfBounds(pos, len(grid), len(grid[0])) &&
+				grid[pos.row][pos.col] {
+				adjacent++
 			}
-			adjacent := 0
-			for _, pos := range getAdjacent(position{rowIdx, colIdx}) {
-				if !outOfBounds(pos, len(grid), len(grid[0])) &&
-					grid[pos.row][pos.col] {
-					adjacent++
-				}
-				if adjacent >= 4 {
-					break
-				}
+			if adjacent >= 4 {
+				break
 			}
-			if adjacent < 4 {
-				accessible++
-			}
+		}
+		if adjacent < 4 {
+			accessible++
+			grid[roll.row][roll.col] = false
+			delete(rolls, roll)
 		}
 	}
 	return accessible
@@ -62,12 +59,21 @@ func main() {
 		inputScanner = bufio.NewScanner(inputFile)
 	}
 	var grid [][]bool
+	rolls := map[position]bool{}
 	for inputScanner.Scan() {
 		var row []bool
-		for _, symbol := range inputScanner.Text() {
+		for colIdx, symbol := range inputScanner.Text() {
 			row = append(row, symbol == '@')
+			if row[len(row)-1] {
+				rolls[position{len(grid), colIdx}] = true
+			}
 		}
 		grid = append(grid, row)
 	}
-	fmt.Println(countAccessible(grid))
+	result, accessible := 0, countAccessible(grid, rolls)
+	for accessible > 0 {
+		result += accessible
+		accessible = countAccessible(grid, rolls)
+	}
+	fmt.Println(result)
 }
